a89e735e40ea589f840fe9b74d785b25854f3f68
Alf Tang <tang.alf@gmail.com>
2013-11-20 12:13:57 +0800

    change comment of variable m

--- nnCostFunction.m
+++ nnCostFunction.m
@@ -22,7 +22,7 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
 
-% Setup some useful variables
+% Number of training examples
 m = size(X, 1);
          
 % You need to return the following variables correctly 
